b3153a5a93b678f9f7a99a6e19923f8bec9adef9
Kohlmann András <metuoku@outlook.com>
2019-02-03 22:47:46 +0100

    server uri

--- mnist_client.py
+++ mnist_client.py
@@ -1,5 +1,6 @@
 import json
 import random
+import sys
 
 from tensorflow import keras
 import requests
@@ -13,12 +14,16 @@ class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
+server_uri = 'localhost:8501'
+if len(sys.argv) > 1:
+    server_uri = sys.argv[1]
+
 rando = random.randint(0, len(test_images) - 1)
 data = json.dumps({"signature_name": "serving_default", "instances": test_images[0:3].tolist()})
 print('Data: {} ... {}'.format(data[:50], data[len(data) - 52:]))
 
 headers = {"content-type": "application/json"}
-json_response = requests.post('http://localhost:8501/v1/models/mnist/1:predict', data=data, headers=headers)
+json_response = requests.post('http://{}/v1/models/mnist/1:predict'.format(server_uri), data=data, headers=headers)
 predictions = json.loads(json_response.text)['predictions']
 
 print('The model thought this was a {} (class {}), and it was actually a {} (class {})'.format(
